7c71b8905b91593a267a822460f9e65d8d049c22
Gurusudhan25 <gurusudhan25@gmail.com>
2023-09-23 12:29:27 +0530

    initial setup

---
+++ apps/quizbox/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'quizbox-fe-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'quizbox';
+}
