0faa620d94a033170edf6fb29c3dabf47f8f6058
Gurusudhan25 <gurusudhan25@gmail.com>
2023-09-26 18:14:58 +0530

    done for header

--- apps/quizbox/src/app/app.routes.ts
+++ apps/quizbox/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  { path: '', pathMatch: 'full', redirectTo: 'quizbox' },
+  { path: 'quizbox', loadChildren: () => import('./landing-page/landing-page.module').then(m => m.LandingPageModule) }
+];
