c5d50f18d17fda5c1e3d4d461b28927241a48a35
Gurusudhan25 <gurusudhan25@gmail.com>
2023-10-07 14:51:32 +0530

    theming done

--- apps/quizbox/src/app/landing-page/landing-page.module.ts
+++ apps/quizbox/src/app/landing-page/landing-page.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page.component';
 import { RouterModule } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [LandingPageComponent],
   imports: [
     CommonModule,
+    MatButtonModule,
     RouterModule.forChild(
       [{ path: '', component: LandingPageComponent }]
     ),
